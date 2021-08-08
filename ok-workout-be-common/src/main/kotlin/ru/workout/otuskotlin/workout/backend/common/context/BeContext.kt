485ee540d4414eba903c274b35df51ddf796ffe3
KonstantinKhan <khan366kos@gmail.com>
2021-08-08 20:28:57 +0500

    Updated BeContext.kt

--- ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/context/BeContext.kt
+++ ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/context/BeContext.kt
@@ -1,13 +1,14 @@
 package ru.workout.otuskotlin.workout.backend.common.context
 
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseIdModel
 import ru.workout.otuskotlin.workout.backend.common.models.ExerciseModel
 import ru.workout.otuskotlin.workout.backend.common.models.IError
 
 data class BeContext(
     var requestId: String = "",
+    var requestExerciseId: ExerciseIdModel = ExerciseIdModel.NONE,
     var requestExercise: ExerciseModel = ExerciseModel(),
     var responseExercise: ExerciseModel = ExerciseModel(),
     var responseExercises: MutableList<ExerciseModel> = mutableListOf(),
     var errors: MutableList<IError> = mutableListOf()
-
 )
