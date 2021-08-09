dc86cd7e02e304c79fc3a2bccbdb4fdd08bbb499
KonstantinKhan <khan366kos@gmail.com>
2021-08-09 08:14:50 +0500

    Created data of Transport2Context.kt

--- ok-workout-be-mapping/src/main/kotlin/ru/workout/otuskotlin/workout/backend/mapping/openapi/Transport2Context.kt
+++ ok-workout-be-mapping/src/main/kotlin/ru/workout/otuskotlin/workout/backend/mapping/openapi/Transport2Context.kt
@@ -1,8 +1,52 @@
 package ru.workout.otuskotlin.workout.backend.mapping.openapi
 
-import ru.otus.otuskotlin.workout.openapi.models.CreateExerciseRequest
+import ru.otus.otuskotlin.workout.openapi.models.*
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseIdModel
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseModel
+
+fun BeContext.setQuery(query: InitExerciseRequest) = apply {
+    requestId = query.requestId ?: ""
+}
 
 fun BeContext.setQuery(query: CreateExerciseRequest) = apply {
-
+    requestId = query.requestId ?: ""
+    requestExercise = query.createExercise?.toModel() ?: ExerciseModel()
 }
+
+fun BeContext.setQuery(query: ReadExerciseRequest) = apply {
+    requestId = query.requestId ?: ""
+    requestExerciseId = ExerciseIdModel(query.readExerciseId ?: "")
+}
+
+fun BeContext.setQuery(query: UpdateExerciseRequest) = apply {
+    requestId = query.requestId ?: ""
+    requestExercise = query.createExercise?.toModel() ?: ExerciseModel()
+}
+
+fun BeContext.setQuery(query: DeleteExerciseRequest) = apply {
+    requestId = query.requestId ?: ""
+    requestExerciseId = ExerciseIdModel(query.deleteExerciseId ?: "")
+}
+
+fun BeContext.setQuery(query: SearchExerciseRequest) = apply {
+    requestId = query.requestId ?: ""
+    // TODO: 09.08.2021 дополнить
+}
+
+private fun CreatableExercise.toModel() = ExerciseModel(
+    title = title ?: "",
+    description = description ?: "",
+    targetMuscleGroup = targetMuscleGroup?.toMutableList() ?: mutableListOf(),
+    synergisticMuscleGroup = synergisticMuscleGroup?.toMutableList() ?: mutableListOf(),
+    executionTechnique = executionTechnique ?: ""
+)
+
+private fun UpdatableExercise.toModel() = ExerciseModel(
+    id = ExerciseIdModel(id ?: ""),
+    title = title ?: "",
+    description = description ?: "",
+    targetMuscleGroup = targetMuscleGroup?.toMutableList() ?: mutableListOf(),
+    synergisticMuscleGroup = synergisticMuscleGroup?.toMutableList() ?: mutableListOf(),
+    executionTechnique = executionTechnique ?: ""
+)
