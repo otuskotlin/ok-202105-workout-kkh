b3ddbdb860c4555fe7ee2d04c19de78ce06a564f
KonstantinKhan <khan366kos@gmail.com>
2021-08-11 10:56:59 +0500

    Created simple tests for CreateWorkoutRequest

--- ok-workout-be-transport-openapi/src/test/kotlin/SerialisationTest.kt
+++ ok-workout-be-transport-openapi/src/test/kotlin/SerialisationTest.kt
@@ -1,39 +1,85 @@
 import com.fasterxml.jackson.databind.ObjectMapper
-import ru.otus.otuskotlin.workout.openapi.models.BaseMessage
-import ru.otus.otuskotlin.workout.openapi.models.CreatableExercise
-import ru.otus.otuskotlin.workout.openapi.models.CreateExerciseRequest
+import ru.otus.otuskotlin.workout.openapi.models.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class SerialisationTest {
     private val requestId = "id:123"
-    private val createRequest = CreateExerciseRequest(
+    private val exercise1 = CreatableExercise(
+        title = "Приседания со штангой",
+        description = "Базовое упражнение",
+        targetMuscleGroup = listOf("Квадрицепсы"),
+        synergisticMuscleGroup = listOf("Большие ягодичные", "Приводящие бедра", "Камбаловидные"),
+        executionTechnique = "При выполнении приседаний можно варьировать положение ног, но обычно рекомендуют ставить носки врозь, а ноги примерно на ширине плеч"
+    )
+    private val createExerciseRequest = CreateExerciseRequest(
         requestId = requestId,
-        createExercise = CreatableExercise(
-            title = "Приседания со штангой",
-            description = "Базовое упражнение",
-            targetMuscleGroup = listOf("Квадрицепсы"),
-            synergisticMuscleGroup = listOf("Большие ягодичные", "Приводящие бедра", "Камбаловидные"),
-            executionTechnique = "При выполнении приседаний можно варьировать положение ног, но обычно рекомендуют ставить носки врозь, а ноги примерно на ширине плеч"
+        createExercise = exercise1
+    )
+    private val createWorkoutRequest = CreateWorkoutRequest(
+        requestId = requestId,
+        createWorkout = CreatableWorkout(
+            date = "2021-08-11",
+            duration = 0.0,
+            recoveryTime = 60.0,
+            modificationWorkout = CreatableWorkout.ModificationWorkout.CLASSIC,
+            exercisesBlock = mutableListOf(
+                ExercisesBlock(
+                    exercise = exercise1,
+                    sets = mutableListOf(
+                        OneSet(
+                            performance = mutableListOf(
+                                Performance(
+                                    weight = 80.0,
+                                    measure = Performance.Measure.KG,
+                                    repetition = 10
+                                )
+                            ),
+                            status = OneSet.Status.PLAN,
+                            modificationExercise = OneSet.ModificationExercise.NONE
+                        )
+                    ),
+                    modificationBlockExercises = ExercisesBlock.ModificationBlockExercises.NONE
+                )
+            )
         )
     )
+
     private val om = ObjectMapper()
 
     @Test
-    fun serialisationTest() {
-        val json = om.writeValueAsString(createRequest)
+    fun serialisationExerciseTest() {
+        val json = om.writeValueAsString(createExerciseRequest)
         println(json)
         assertTrue("json must contain discriminator") {
-            json.contains(""""messageType":"${createRequest::class.simpleName}"""")
+            json.contains(""""messageType":"${createExerciseRequest::class.simpleName}"""")
         }
     }
 
     @Test
-    fun deserializationTest() {
-        val json = om.writeValueAsString(createRequest)
+    fun deserializationExerciseTest() {
+        val json = om.writeValueAsString(createExerciseRequest)
         val deserialized = om.readValue(json, BaseMessage::class.java) as CreateExerciseRequest
         println(deserialized)
         assertEquals(requestId, deserialized.requestId)
     }
+
+    @Test
+    fun serializationWorkoutTest() {
+        val json = om.writeValueAsString(createWorkoutRequest)
+        println(json)
+        assertTrue("json must contain discriminator") {
+            json.contains(""""messageType":"${createWorkoutRequest::class.simpleName}"""")
+        }
+    }
+
+    @Test
+    fun deserializationWorkoutTest() {
+        val json = om.writeValueAsString(createWorkoutRequest)
+        val deserialized = om.readValue(json, BaseMessage::class.java) as CreateWorkoutRequest
+        println(deserialized)
+        assertEquals(requestId, deserialized.requestId)
+    }
+
 }
