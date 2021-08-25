056aed5e902ec563204ae757d884eec15c26df23
KonstantinKhan <khan366kos@gmail.com>
2021-08-25 21:31:30 +0500

    Updated requests.

--- ok-workout-be-stubs/src/main/kotlin/WorkoutStub.kt
+++ ok-workout-be-stubs/src/main/kotlin/WorkoutStub.kt
@@ -1,22 +1,25 @@
 import ru.workout.otuskotlin.workout.backend.common.convertToInstant
-import ru.workout.otuskotlin.workout.backend.common.models.ExercisesBlockModel
-import ru.workout.otuskotlin.workout.backend.common.models.OneSetModel
-import ru.workout.otuskotlin.workout.backend.common.models.PerformanceModel
-import ru.workout.otuskotlin.workout.backend.common.models.WorkoutModel
+import ru.workout.otuskotlin.workout.backend.common.models.*
 
 object WorkoutStub {
     private val workoutStubReady = WorkoutModel(
         workoutDate = "2021-08-23 14:00 +03".convertToInstant(),
         recoveryTime = 120.0,
-        exercisesBlock = mutableListOf(ExercisesBlockModel(
-            ExerciseStub.getModelExercise(),
-            sets = mutableListOf(OneSetModel(
-                performance = mutableListOf(PerformanceModel(
-                    weight = 100.0,
-                    repetition = 10
-                ))
-            ))
-        ))
+        exercisesBlock = mutableListOf(
+            ExercisesBlockModel(
+                ExerciseStub.getModelExercise(),
+                sets = mutableListOf(
+                    OneSetModel(
+                        performance = mutableListOf(
+                            PerformanceModel(
+                                weight = 100.0,
+                                repetition = 10
+                            )
+                        )
+                    )
+                )
+            )
+        )
     )
 
     fun getModelWorkout() = workoutStubReady
