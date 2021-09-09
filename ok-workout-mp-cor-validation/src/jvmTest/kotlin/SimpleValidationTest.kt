5ad46ce7e9a1f71488743fb73ca3da3891b773f0
KonstantinKhan <khan366kos@gmail.com>
2021-09-09 15:15:34 +0500

    Copied a simple validation test.

---
+++ ok-workout-mp-cor-validation/src/jvmTest/kotlin/SimpleValidationTest.kt
@@ -0,0 +1,32 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SimpleValidationTest {
+
+    @Test
+    fun pipelineValidation() {
+        val chain = chain<TestContext> {
+            validation {
+                errorHandler { validationResult ->
+                    if (!validationResult.isSuccess) {
+                        errors.addAll(validationResult.errors)
+                    }
+                }
+                validate<String?> { validator(ValidatorStringNonEmpty()); on { x } }
+                validate<String?> { validator(ValidatorStringNonEmpty()); on { y } }
+            }
+        }
+        val c = TestContext()
+
+        runBlocking {
+            chain.build().exec(c)
+            assertEquals(2, c.errors.size)
+        }
+    }
+
+    data class TestContext(
+        val x: String = "",
+        val y: String = "",
+        val errors: MutableList<IValidationError> = mutableListOf()
+    )
+}
