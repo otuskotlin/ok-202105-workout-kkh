77e3f518d5faa028488c73e8f35de5504aa0be75
KonstantinKhan <khan366kos@gmail.com>
2021-09-10 23:34:20 +0500

    Updated ValidationWorker.kt

--- ok-workout-mp-cor-validation/src/commonMain/kotlin/workers/ValidationWorker.kt
+++ ok-workout-mp-cor-validation/src/commonMain/kotlin/workers/ValidationWorker.kt
@@ -2,7 +2,16 @@ package workers
 
 import ICorChainDsl
 import ValidationBuilder
+import handlers.worker
 
 fun <C> ICorChainDsl<C>.validation(block: ValidationBuilder<C>.() -> Unit) {
+    worker {
+        title = "Validation"
+        description = "Validation of logic"
 
+        handle {
+            ValidationBuilder<C>().apply(block).build().exec(this)
+        }
+
+    }
 }
