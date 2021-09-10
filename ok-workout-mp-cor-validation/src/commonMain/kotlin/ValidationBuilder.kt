6ec19424c083b40ca78f757a2c2e3e4999f2295d
KonstantinKhan <khan366kos@gmail.com>
2021-09-10 23:21:53 +0500

    Created PipelineValidation.kt Updated ValidationBuilder.kt

--- ok-workout-mp-cor-validation/src/commonMain/kotlin/ValidationBuilder.kt
+++ ok-workout-mp-cor-validation/src/commonMain/kotlin/ValidationBuilder.kt
@@ -2,15 +2,17 @@ import ru.otus.otuskotlin.workout.validation.ValidationResult
 
 class ValidationBuilder<C> {
 
-    fun errorHandler(block: C.(ValidationResult) -> Unit) {
+    private var errorHandler: C.(ValidationResult) -> Unit = {}
+    private var validators: MutableList<IValidationOperation<C, *>> = mutableListOf()
 
+    fun errorHandler(block: C.(ValidationResult) -> Unit) {
+        errorHandler = block
     }
 
     fun <T> validate(block: ValidationOperationBuilder<C, T>.() -> Unit) {
-
+        val validator = ValidationOperationBuilder<C, T>(errorHandler).apply(block)
+        validators.add(validator.build())
     }
 
-    fun build() {
-
-    }
+    fun build() = PipelineValidation(validators)
 }
