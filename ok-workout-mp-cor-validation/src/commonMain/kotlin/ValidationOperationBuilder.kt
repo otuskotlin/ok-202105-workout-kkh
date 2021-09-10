b06c235544e410f750f975dce9b10ab0f0fe09de
KonstantinKhan <khan366kos@gmail.com>
2021-09-10 16:53:14 +0500

    Updated ValidationOperationBuilder.kt and DefaultValidationOperation.kt.

--- ok-workout-mp-cor-validation/src/commonMain/kotlin/ValidationOperationBuilder.kt
+++ ok-workout-mp-cor-validation/src/commonMain/kotlin/ValidationOperationBuilder.kt
@@ -1,17 +1,24 @@
 import ru.otus.otuskotlin.workout.validation.IValidator
-import workers.DefaultValidationOperation
+import ru.otus.otuskotlin.workout.validation.ValidationResult
 
-class ValidationOperationBuilder<C, T> {
+class ValidationOperationBuilder<C, T>(
+    private var errorHandler: C.(ValidationResult) -> Unit = {}
+) {
+
+    private lateinit var validator: IValidator<T>
+    private lateinit var value: C.() -> T
 
     fun validator(validator: IValidator<T>) {
-
+        this.validator = validator
     }
 
-    fun on(block: C.() -> Unit) {
-
+    fun on(block: C.() -> T) {
+        value = block
     }
 
     fun build() = DefaultValidationOperation<C, T>(
-
+        value = value,
+        validator = validator,
+        errorHandler = errorHandler
     )
 }
--- ok-workout-mp-cor-validation/src/commonMain/kotlin/workers/DefaultValidationOperation.kt
+++
@@ -1,9 +0,0 @@
-package workers
-
-import IValidationOperation
-
-class DefaultValidationOperation<C, T>: IValidationOperation<C, T> {
-    override fun exec(context: C) {
-        TODO("Not yet implemented")
-    }
-}
