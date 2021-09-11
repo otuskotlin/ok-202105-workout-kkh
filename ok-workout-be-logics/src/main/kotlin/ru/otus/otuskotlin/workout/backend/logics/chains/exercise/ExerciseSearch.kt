1020a4ffb8cf5eaffeac497b5581186e0fd0ecba
KonstantinKhan <khan366kos@gmail.com>
2021-09-11 23:16:55 +0500

    Added the validations in ExerciseSearch.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseSearch.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseSearch.kt
@@ -3,9 +3,11 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.exercise
 import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.excercise.exerciseSearchStub
+import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
+import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 
 object ExerciseSearch : ICorExec<BeContext> by chain<BeContext>({
@@ -16,9 +18,19 @@ object ExerciseSearch : ICorExec<BeContext> by chain<BeContext>({
 
     chainInitWorker("Инициализация чейна")
 
-    // validation
-
     exerciseSearchStub(title = "Обработка стабкейса для SEARCH")
+
+    // validation
+    validationLogics {
+        validate<String?> {
+            on { requestId }
+            validator(StringNonEmptyValidator(field = "requestId"))
+        }
+        validate<String?> {
+            on { requestSearchExercise }
+            validator(StringNonEmptyValidator(field = "requestSearchExercise"))
+        }
+    }
 
     // db working
 
