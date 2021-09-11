2d5953bef1f7b44a8ade1ae82da5f2da14d69f3a
KonstantinKhan <khan366kos@gmail.com>
2021-09-11 09:42:07 +0500

    Updated ExerciseCreate.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseCreate.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseCreate.kt
@@ -3,14 +3,12 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.exercise
 import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.excercise.exerciseCreateStub
+import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
-import ru.otus.otuskotlin.workout.validation.IValidationFieldError
 import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
-import ru.workout.otuskotlin.workout.backend.common.models.CommonErrorModel
-import workers.validation
 
 object ExerciseCreate : ICorExec<BeContext> by chain<BeContext>({
     checkOperationWorker(
@@ -22,23 +20,17 @@ object ExerciseCreate : ICorExec<BeContext> by chain<BeContext>({
 
     exerciseCreateStub(title = "Обработка стабкейса для CREATE")
 
-    validation {
-        errorHandler { validationResult ->
-            if (validationResult.isSuccess) return@errorHandler
-            val errors = validationResult.errors.map {
-                CommonErrorModel(
-                    message = title,
-                    field = if (it is IValidationFieldError) it.field else ""
-                )
-            }
-            this.errors.addAll(errors)
-        }
+    validationLogics {
         validate<String?> {
             on { requestExercise.title }
             validator(StringNonEmptyValidator(field = "title"))
         }
+        validate<String?> {
+            on { requestExercise.description }
+            validator(StringNonEmptyValidator(field = "description"))
+        }
     }
-    
+
     // db working
 
     prepareAnswer("Подготовка ответа")
