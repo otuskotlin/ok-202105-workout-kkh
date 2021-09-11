af3050476969629bcc946f07c4ba9495eb421052
KonstantinKhan <khan366kos@gmail.com>
2021-09-11 21:43:06 +0500

    Updated ExerciseCrudValidationTest.kt

--- ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/ExerciseCrudValidationTest.kt
+++ ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/ExerciseCrudValidationTest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 import ru.workout.otuskotlin.workout.backend.common.context.CorStatus
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseIdModel
 import ru.workout.otuskotlin.workout.backend.common.models.MpStubCases
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -15,10 +16,12 @@ class ExerciseCrudValidationTest {
     @Test
     fun createExerciseSuccess() {
         val context = BeContext(
+            requestId = "rID:00111",
             stubCase = MpStubCases.SUCCESS,
             requestExercise = ExerciseStub.getModelExercise(),
             operation = BeContext.MpOperations.CREATE
         )
+
         runBlocking { crud.create(context) }
 
         assertEquals(CorStatus.SUCCESS, context.status)
@@ -29,7 +32,6 @@ class ExerciseCrudValidationTest {
     fun createExerciseFailing() {
 
         val context = BeContext(
-            requestId = "",
             stubCase = MpStubCases.SUCCESS,
             requestExercise = ExerciseStub.getModelExercise() {
                 title = ""
@@ -44,6 +46,35 @@ class ExerciseCrudValidationTest {
 
         assertEquals(CorStatus.ERROR, context.status)
         assertEquals(6, context.errors.size)
+    }
 
+    @Test
+    fun readExerciseSuccess() {
+        val context = BeContext(
+            requestId = "rID:00111",
+            requestExerciseId = ExerciseIdModel("eID:00011"),
+            stubCase = MpStubCases.SUCCESS,
+            requestExercise = ExerciseStub.getModelExercise(),
+            operation = BeContext.MpOperations.READ
+        )
+
+        runBlocking { crud.read(context) }
+
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertTrue(context.errors.isEmpty())
+    }
+
+    @Test
+    fun readExerciseFailing() {
+
+        val context = BeContext(
+            stubCase = MpStubCases.SUCCESS,
+            requestExercise = ExerciseStub.getModelExercise(),
+            operation = BeContext.MpOperations.READ
+        )
+        runBlocking { crud.read(context) }
+
+        assertEquals(CorStatus.ERROR, context.status)
+        assertEquals(2, context.errors.size)
     }
 }
