ac3a4754d7a7c3e90aeb56de2121790ce61b44ae
KonstantinKhan <khan366kos@gmail.com>
2021-09-11 09:40:45 +0500

    Created extension to validation worker.

---
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/helpers/Validation.kt
@@ -0,0 +1,24 @@
+package ru.otus.otuskotlin.workout.backend.logics.helpers
+
+import ValidationBuilder
+import handlers.CorChainDsl
+import ru.otus.otuskotlin.workout.validation.IValidationFieldError
+import ru.workout.otuskotlin.workout.backend.common.context.BeContext
+import ru.workout.otuskotlin.workout.backend.common.context.CorStatus
+import ru.workout.otuskotlin.workout.backend.common.models.CommonErrorModel
+import workers.validation
+
+fun CorChainDsl<BeContext>.validationLogics(block: ValidationBuilder<BeContext>.() -> Unit) = validation {
+    errorHandler { validationResult ->
+        if (validationResult.isSuccess) return@errorHandler
+        val errors = validationResult.errors.map {
+            CommonErrorModel(
+                message = title,
+                field = if (it is IValidationFieldError) it.field else ""
+            )
+        }
+        this.errors.addAll(errors)
+        status = CorStatus.FAILING
+    }
+    block()
+}
