46e6ad4c733505d95f85186e56c4207407d7eb49
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 09:46:16 +0500

    Added the validations to WorkoutReadStub.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutRead.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutRead.kt
@@ -3,9 +3,12 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.workout
 import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.workout.workoutReadStub
+import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
+import ru.otus.otuskotlin.workout.validation.validators.ListNonEmptyValidator
+import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 
 object WorkoutRead : ICorExec<BeContext> by chain<BeContext>({
@@ -16,9 +19,19 @@ object WorkoutRead : ICorExec<BeContext> by chain<BeContext>({
 
     chainInitWorker("Инициализация чейна")
 
-    // validation
-
     workoutReadStub(title = "Обработка стабкейса для READ")
+
+    // validation
+    validationLogics {
+        validate<String?> {
+            on { requestId }
+            validator(StringNonEmptyValidator(field = "requestId"))
+        }
+        validate<String?> {
+            on { requestWorkoutId.asString() }
+            validator(StringNonEmptyValidator(field = "requestWorkoutId"))
+        }
+    }
 
     // db working
 
