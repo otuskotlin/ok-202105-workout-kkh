b1b701e5773185419a18b0eb41a56d807be4bc1b
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 09:48:22 +0500

    Added read success and failing tests to WorkoutCrudValidationTest.kt

--- ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/WorkoutCrudValidationTest.kt
+++ ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/WorkoutCrudValidationTest.kt
@@ -9,6 +9,7 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class WorkoutCrudValidationTest {
+
     @Test
     fun createWorkoutSuccess() {
         val crud = WorkoutCrud()
@@ -27,6 +28,37 @@ class WorkoutCrudValidationTest {
 
     @Test
     fun createWorkoutFailing() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "",
+            stubCase = MpStubCases.SUCCESS,
+            operation = BeContext.MpOperations.CREATE
+        )
+
+        runBlocking { crud.create(context) }
+
+        assertEquals(CorStatus.ERROR, context.status)
+        assertEquals(2, context.errors.size)
+    }
+
+    @Test
+    fun readWorkoutSuccess() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "rID:00111",
+            stubCase = MpStubCases.SUCCESS,
+            requestWorkout = WorkoutStub.getModelWorkout(),
+            operation = BeContext.MpOperations.READ
+        )
+
+        runBlocking { crud.read(context) }
+
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertTrue(context.errors.isEmpty())
+    }
+
+    @Test
+    fun readWorkoutFailing() {
         val crud = WorkoutCrud()
         val context = BeContext(
             requestId = "",
