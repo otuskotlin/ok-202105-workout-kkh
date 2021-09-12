dacb99ffa769fd7cd9f3273f345a2a88cfca57f8
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 11:54:23 +0500

    Added the validations to WorkoutDelete.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutDelete.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutDelete.kt
@@ -3,9 +3,11 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.workout
 import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.workout.workoutDeleteStub
+import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
+import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 
 object WorkoutDelete : ICorExec<BeContext> by chain<BeContext>({
@@ -16,9 +18,19 @@ object WorkoutDelete : ICorExec<BeContext> by chain<BeContext>({
 
     chainInitWorker("Инициализация чейна")
 
-    // validation
-
     workoutDeleteStub(title = "Обработка стабкейса для DELETE")
+
+    // validation
+    validationLogics {
+        validate<String?> {
+            on { requestId }
+            validator(StringNonEmptyValidator(field = "requestId"))
+        }
+        validate<String?> {
+            on { requestWorkoutId.asString() }
+            validator(StringNonEmptyValidator(field = "requestWorkoutId"))
+        }
+    }
 
     // db working
 
