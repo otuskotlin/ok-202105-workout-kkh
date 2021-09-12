13ed26a1ef8a42b4434533cfb13af39fb6e7a70f
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 14:19:42 +0500

    Updated WorkoutCrudValidationTest.kt

--- ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/WorkoutCrudValidationTest.kt
+++ ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/WorkoutCrudValidationTest.kt
@@ -71,4 +71,99 @@ class WorkoutCrudValidationTest {
         assertEquals(CorStatus.ERROR, context.status)
         assertEquals(2, context.errors.size)
     }
+
+    @Test
+    fun updatedWorkoutSuccess() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "rID:00111",
+            stubCase = MpStubCases.SUCCESS,
+            requestWorkout = WorkoutStub.getModelWorkout(),
+            operation = BeContext.MpOperations.UPDATE
+        )
+
+        runBlocking { crud.update(context) }
+
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertTrue(context.errors.isEmpty())
+    }
+
+    @Test
+    fun updateWorkoutFailing() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "",
+            stubCase = MpStubCases.SUCCESS,
+            operation = BeContext.MpOperations.UPDATE
+        )
+
+        runBlocking { crud.update(context) }
+
+        assertEquals(CorStatus.ERROR, context.status)
+        assertEquals(3, context.errors.size)
+    }
+
+    @Test
+    fun deleteWorkoutSuccess() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "rID:00111",
+            stubCase = MpStubCases.SUCCESS,
+            requestWorkout = WorkoutStub.getModelWorkout(),
+            operation = BeContext.MpOperations.DELETE
+        )
+
+        runBlocking { crud.delete(context) }
+
+        context.errors.forEach { println(it.message) }
+
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertTrue(context.errors.isEmpty())
+    }
+
+    @Test
+    fun deleteWorkoutFailing() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "",
+            stubCase = MpStubCases.SUCCESS,
+            operation = BeContext.MpOperations.DELETE
+        )
+
+        runBlocking { crud.delete(context) }
+
+        assertEquals(CorStatus.ERROR, context.status)
+        assertEquals(2, context.errors.size)
+    }
+
+    @Test
+    fun searchWorkoutSuccess() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "rID:00111",
+            stubCase = MpStubCases.SUCCESS,
+            requestWorkout = WorkoutStub.getModelWorkout(),
+            operation = BeContext.MpOperations.SEARCH
+        )
+
+        runBlocking { crud.search(context) }
+
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertTrue(context.errors.isEmpty())
+    }
+
+    @Test
+    fun searchWorkoutFailing() {
+        val crud = WorkoutCrud()
+        val context = BeContext(
+            requestId = "",
+            stubCase = MpStubCases.SUCCESS,
+            operation = BeContext.MpOperations.SEARCH
+        )
+
+        runBlocking { crud.search(context) }
+
+        assertEquals(CorStatus.ERROR, context.status)
+        assertEquals(1, context.errors.size)
+    }
 }
