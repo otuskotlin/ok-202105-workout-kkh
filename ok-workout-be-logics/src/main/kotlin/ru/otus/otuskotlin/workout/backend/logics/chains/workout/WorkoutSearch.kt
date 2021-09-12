e040c75dd90a007a6fc5ea5118ac93f3bd61574d
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 14:17:58 +0500

    Added the validations to WorkoutSearch.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutSearch.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/workout/WorkoutSearch.kt
@@ -3,9 +3,11 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.workout
 import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.workout.workoutSearchStub
+import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
+import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 
 object WorkoutSearch : ICorExec<BeContext> by chain<BeContext>({
@@ -16,9 +18,15 @@ object WorkoutSearch : ICorExec<BeContext> by chain<BeContext>({
 
     chainInitWorker("Инициализация чейна")
 
-    // validation
-
     workoutSearchStub(title = "Обработка стабкейса для Search")
+
+    // validation
+    validationLogics {
+        validate<String?> {
+            on { requestId }
+            validator(StringNonEmptyValidator(field = "requestId"))
+        }
+    }
 
     // db working
 
