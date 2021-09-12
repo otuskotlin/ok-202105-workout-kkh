69933d700ccd549c9c8f82783ec3f3d199de1d64
KonstantinKhan <khan366kos@gmail.com>
2021-09-12 15:02:14 +0500

    Fixed bug in WorkoutChainOfExercisesStub.kt

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/stubs/workout/WorkoutChainOfExercisesStub.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/stubs/workout/WorkoutChainOfExercisesStub.kt
@@ -20,7 +20,6 @@ internal fun CorChainDsl<BeContext>.workoutChainOfExerciseStub(title: String) = 
             stubCase == MpStubCases.SUCCESS
         }
         handle {
-            requestWorkoutId = requestWorkout.idWorkout
             responseExercises = WorkoutStub.getChainOfExercises()
             status = CorStatus.FINISHING
         }
