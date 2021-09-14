713e0992a4f88cda6b4dee19b58d625b04534b35
KonstantinKhan <khan366kos@gmail.com>
2021-09-14 14:47:59 +0500

    Added HandleRoute.kt - refactoring. Updated ExerciseController.kt and WorkoutController.kt

--- ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/ExerciseController.kt
+++ ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/ExerciseController.kt
@@ -4,92 +4,131 @@ import ExerciseService
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.response.*
+import ru.otus.otuskotlin.workout.helpers.handleRoute
 import ru.otus.otuskotlin.workout.openapi.models.*
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 import java.time.Instant
 
 suspend fun ApplicationCall.initExercise(exerciseService: ExerciseService) {
-    val initExerciseRequest = receive<InitExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.INIT
-    )
 
-    val result = try {
-        exerciseService.initExercise(context, initExerciseRequest)
-    } catch (e: Exception) {
-        exerciseService.errorExercise(context, e) as InitExerciseResponse
+    handleRoute<InitExerciseRequest, InitExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.INIT
+        exerciseService.initExercise(this, request)
     }
-    respond(result)
+
+//    val initExerciseRequest = receive<InitExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.INIT
+//    )
+//
+//    val result = try {
+//        exerciseService.initExercise(context, initExerciseRequest)
+//    } catch (e: Exception) {
+//        exerciseService.errorExercise(context, e) as InitExerciseResponse
+//    }
+//    respond(result)
+
 }
 
-suspend fun ApplicationCall.createExercise(exerciseService: ExerciseService) {
-    val createExerciseRequest = receive<CreateExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.CREATE
-    )
 
-    val result = try {
-        exerciseService.createExercise(context, createExerciseRequest)
-    } catch (e: Throwable) {
-        exerciseService.errorExercise(context, e) as CreateExerciseResponse
+suspend fun ApplicationCall.createExercise(exerciseService: ExerciseService) {
+
+    handleRoute<CreateExerciseRequest, CreateExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.CREATE
+        exerciseService.createExercise(this, request)
     }
-    respond(result)
+
+//    val createExerciseRequest = receive<CreateExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.CREATE
+//    )
+//
+//    val result = try {
+//        exerciseService.createExercise(context, createExerciseRequest)
+//    } catch (e: Throwable) {
+//        exerciseService.errorExercise(context, e) as CreateExerciseResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.readExercise(exerciseService: ExerciseService) {
-    val readExerciseRequest = receive<ReadExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.READ
-    )
-    val result = try {
-        exerciseService.readExercise(context, readExerciseRequest)
-    } catch (e: Throwable) {
-        exerciseService.errorExercise(context, e) as ReadExerciseResponse
+
+    handleRoute<ReadExerciseRequest, ReadExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.READ
+        exerciseService.readExercise(this, request)
     }
-    respond(result)
+
+//    val readExerciseRequest = receive<ReadExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.READ
+//    )
+//    val result = try {
+//        exerciseService.readExercise(context, readExerciseRequest)
+//    } catch (e: Throwable) {
+//        exerciseService.errorExercise(context, e) as ReadExerciseResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.updateExercise(exerciseService: ExerciseService) {
-    val updateExerciseRequest = receive<UpdateExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.UPDATE
-    )
-    val result = try {
-        exerciseService.updateExercise(context, updateExerciseRequest)
-    } catch (e: Throwable) {
-        exerciseService.errorExercise(context, e) as UpdateExerciseResponse
+
+    handleRoute<UpdateExerciseRequest, UpdateExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.UPDATE
+        exerciseService.updateExercise(this, request)
     }
-    respond(result)
+
+//    val updateExerciseRequest = receive<UpdateExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.UPDATE
+//    )
+//    val result = try {
+//        exerciseService.updateExercise(context, updateExerciseRequest)
+//    } catch (e: Throwable) {
+//        exerciseService.errorExercise(context, e) as UpdateExerciseResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.deleteExercise(exerciseService: ExerciseService) {
-    val deleteExerciseRequest = receive<DeleteExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.DELETE
-    )
-    val result = try {
-        exerciseService.deleteExercise(context, deleteExerciseRequest)
-    } catch (e: Throwable) {
-        exerciseService.errorExercise(context, e) as DeleteExerciseResponse
+
+    handleRoute<DeleteExerciseRequest, DeleteExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.DELETE
+        exerciseService.deleteExercise(this, request)
     }
-    respond(result)
+
+//    val deleteExerciseRequest = receive<DeleteExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.DELETE
+//    )
+//    val result = try {
+//        exerciseService.deleteExercise(context, deleteExerciseRequest)
+//    } catch (e: Throwable) {
+//        exerciseService.errorExercise(context, e) as DeleteExerciseResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.searchExercise(exerciseService: ExerciseService) {
-    val searchExerciseRequest = receive<SearchExerciseRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.SEARCH
-    )
-    val result = try {
-        exerciseService.searchExercise(context, searchExerciseRequest)
-    } catch (e: Throwable) {
-        exerciseService.errorExercise(context, e) as SearchExerciseResponse
+
+    handleRoute<SearchExerciseRequest, SearchExerciseResponse> { request ->
+        this.operation = BeContext.MpOperations.SEARCH
+        exerciseService.searchExercise(this, request)
     }
-    respond(result)
+
+//    val searchExerciseRequest = receive<SearchExerciseRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.SEARCH
+//    )
+//    val result = try {
+//        exerciseService.searchExercise(context, searchExerciseRequest)
+//    } catch (e: Throwable) {
+//        exerciseService.errorExercise(context, e) as SearchExerciseResponse
+//    }
+//    respond(result)
 }
--- ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/WorkoutController.kt
+++ ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/WorkoutController.kt
@@ -4,104 +4,147 @@ import WorkoutService
 import io.ktor.application.*
 import io.ktor.request.*
 import io.ktor.response.*
+import ru.otus.otuskotlin.workout.helpers.handleRoute
 import ru.otus.otuskotlin.workout.openapi.models.*
 import ru.workout.otuskotlin.workout.backend.common.context.BeContext
 import java.time.Instant
 
 suspend fun ApplicationCall.initWorkout(workoutService: WorkoutService) {
-    val initWorkoutRequest = receive<InitWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.INIT
-    )
-    val result = try {
-        workoutService.initWorkout(context, initWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as InitWorkoutResponse
+
+    handleRoute<InitWorkoutRequest, InitWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.INIT
+        workoutService.initWorkout(this, request)
     }
-    respond(result)
+
+//    val initWorkoutRequest = receive<InitWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.INIT
+//    )
+//    val result = try {
+//        workoutService.initWorkout(context, initWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as InitWorkoutResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.createWorkout(workoutService: WorkoutService) {
-    val createWorkoutRequest = receive<CreateWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.CREATE
-    )
-    val result = try {
-        workoutService.createWorkout(context, createWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as CreateWorkoutResponse
+
+    handleRoute<CreateWorkoutRequest, CreateWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.CREATE
+        workoutService.createWorkout(this, request)
     }
-    respond(result)
+
+//    val createWorkoutRequest = receive<CreateWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.CREATE
+//    )
+//    val result = try {
+//        workoutService.createWorkout(context, createWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as CreateWorkoutResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.readWorkout(workoutService: WorkoutService) {
-    val readWorkoutRequest = receive<ReadWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.READ
-    )
-    val result = try {
-        workoutService.readWorkout(context, readWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as ReadWorkoutResponse
+
+    handleRoute<ReadWorkoutRequest, ReadWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.READ
+        workoutService.readWorkout(this, request)
     }
-    respond(result)
+
+//    val readWorkoutRequest = receive<ReadWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.READ
+//    )
+//    val result = try {
+//        workoutService.readWorkout(context, readWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as ReadWorkoutResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.updateWorkout(workoutService: WorkoutService) {
-    val updateWorkoutRequest = receive<UpdateWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.UPDATE
-    )
-    val result = try {
-        workoutService.updateWorkout(context, updateWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as UpdateWorkoutResponse
+
+    handleRoute<UpdateWorkoutRequest, UpdateWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.UPDATE
+        workoutService.updateWorkout(this, request)
     }
-    respond(result)
+
+//    val updateWorkoutRequest = receive<UpdateWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.UPDATE
+//    )
+//    val result = try {
+//        workoutService.updateWorkout(context, updateWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as UpdateWorkoutResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.deleteWorkout(workoutService: WorkoutService) {
-    val deleteWorkoutRequest = receive<DeleteWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.DELETE
-    )
-    val result = try {
-        workoutService.deleteWorkout(context, deleteWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as DeleteExerciseResponse
+
+    handleRoute<DeleteWorkoutRequest, DeleteWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.DELETE
+        workoutService.deleteWorkout(this, request)
     }
-    respond(result)
+
+//    val deleteWorkoutRequest = receive<DeleteWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.DELETE
+//    )
+//    val result = try {
+//        workoutService.deleteWorkout(context, deleteWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as DeleteExerciseResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.searchWorkout(workoutService: WorkoutService) {
-    val searchWorkoutRequest = receive<SearchWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.SEARCH
-    )
-    val result = try {
-        workoutService.searchWorkout(context, searchWorkoutRequest)
-    } catch (e: Throwable) {
-        workoutService.errorWorkout(context, e) as SearchWorkoutResponse
+
+    handleRoute<SearchWorkoutRequest, SearchWorkoutResponse> { request ->
+        this.operation = BeContext.MpOperations.SEARCH
+        workoutService.searchWorkout(this, request)
     }
-    respond(result)
+
+//    val searchWorkoutRequest = receive<SearchWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.SEARCH
+//    )
+//    val result = try {
+//        workoutService.searchWorkout(context, searchWorkoutRequest)
+//    } catch (e: Throwable) {
+//        workoutService.errorWorkout(context, e) as SearchWorkoutResponse
+//    }
+//    respond(result)
 }
 
 suspend fun ApplicationCall.chainOfExercises(workoutService: WorkoutService) {
-    val readWorkoutRequest = receive<ReadWorkoutRequest>()
-    val context = BeContext(
-        startTime = Instant.now(),
-        operation = BeContext.MpOperations.CHAIN_OF_EXERCISES
-    )
-    val result = try {
-        workoutService.chainOfExercises(context, readWorkoutRequest)
-    } catch (e: Exception) {
-        workoutService.errorWorkout(context, e) as ChainOfExercisesResponse
+
+    handleRoute<ReadWorkoutRequest, ChainOfExercisesResponse> { request ->
+        this.operation = BeContext.MpOperations.CHAIN_OF_EXERCISES
+        workoutService.chainOfExercises(this, request)
     }
-    respond(result)
+
+//    val readWorkoutRequest = receive<ReadWorkoutRequest>()
+//    val context = BeContext(
+//        startTime = Instant.now(),
+//        operation = BeContext.MpOperations.CHAIN_OF_EXERCISES
+//    )
+//    val result = try {
+//        workoutService.chainOfExercises(context, readWorkoutRequest)
+//    } catch (e: Exception) {
+//        workoutService.errorWorkout(context, e) as ChainOfExercisesResponse
+//    }
+//    respond(result)
 }
