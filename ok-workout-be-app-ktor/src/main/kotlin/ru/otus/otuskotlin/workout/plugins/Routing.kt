40da74313cbabe8ebe76e4314efca66a7092eeea
KonstantinKhan <khan366kos@gmail.com>
2021-09-26 19:00:54 +0500

    Added websocketExercise and websocketWorkout.

--- ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/plugins/Routing.kt
+++ ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/plugins/Routing.kt
@@ -2,12 +2,15 @@ package ru.otus.otuskotlin.workout.plugins
 
 import ExerciseService
 import WorkoutService
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import ru.otus.otuskotlin.workout.backend.logics.ExerciseCrud
 import ru.otus.otuskotlin.workout.backend.logics.WorkoutCrud
 import ru.otus.otuskotlin.workout.exercise
+import ru.otus.otuskotlin.workout.websocketExercise
+import ru.otus.otuskotlin.workout.websocketWorkout
 import ru.otus.otuskotlin.workout.workout
 
 fun Application.configRouting() {
@@ -15,14 +18,18 @@ fun Application.configRouting() {
     val crudExercise = ExerciseCrud()
     val crudWorkout = WorkoutCrud()
     val exerciseService = ExerciseService(crudExercise)
-    
+    val objectMapper = jacksonObjectMapper()
+
     val workoutService = WorkoutService(crudWorkout)
 
     routing {
         get("/") {
             call.respondText("Hello, Ktor!")
         }
+
         exercise(exerciseService)
         workout(workoutService)
+        websocketExercise(objectMapper, exerciseService)
+        websocketWorkout(objectMapper, workoutService)
     }
 }
