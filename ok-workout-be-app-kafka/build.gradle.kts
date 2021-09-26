b19b69dbbf568fa3715e826eca1841dfaa519168
KonstantinKhan <khan366kos@gmail.com>
2021-09-26 10:40:40 +0500

    Updated version of dependencies.

--- ok-workout-be-app-kafka/build.gradle.kts
+++ ok-workout-be-app-kafka/build.gradle.kts
@@ -13,7 +13,6 @@ docker {
         mainClassName.set(application.mainClass.get())
         baseImage.set("adoptopenjdk/openjdk11:alpine-jre")
         maintainer.set("(c) Otus")
-//        ports.set(listOf(8080))
         val imageName = project.name
         images.set(
             listOf(
@@ -29,8 +28,11 @@ dependencies {
 
     val kafkaVersion: String by project
     val coroutinesVersion: String by project
+    val kotlinStdLibVersion: String by project
+    val kotlinTestJunitVersion: String by project
+    val slf4jVersion: String by project
 
-    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinStdLibVersion")
 
     implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
@@ -43,5 +45,7 @@ dependencies {
 
     implementation(project(":ok-workout-be-logics"))
 
-    testImplementation(kotlin("test-junit"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinTestJunitVersion")
+
+    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
 }
