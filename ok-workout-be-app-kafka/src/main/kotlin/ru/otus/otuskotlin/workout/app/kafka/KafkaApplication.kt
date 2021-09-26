2d4e5fd7ec8661141badc107e9e70c1a988bba58
KonstantinKhan <khan366kos@gmail.com>
2021-09-26 10:41:23 +0500

    Updated KafkaApplication.kt

--- ok-workout-be-app-kafka/src/main/kotlin/ru/otus/otuskotlin/workout/app/kafka/KafkaApplication.kt
+++ ok-workout-be-app-kafka/src/main/kotlin/ru/otus/otuskotlin/workout/app/kafka/KafkaApplication.kt
@@ -27,30 +27,37 @@ class KafkaApplication(private val config: AppKafkaConfig) {
         try {
             consumer.subscribe(config.kafkaTopicsIn)
             while (process.get()) {
-                val context = BeContext(
-                    startTime = Instant.now()
-                )
                 try {
                     val records: ConsumerRecords<String, String> = consumer.poll(Duration.ofSeconds(1))
                     records.forEach { record: ConsumerRecord<String, String> ->
-                        println("record: ${record.value()}")
-                        val service = if (record.value().contains("ExerciseRequest")) {
-                            services[0]
-                        } else {
-                            services[1]
-                        }
-                        val request =
-                            withContext(Dispatchers.IO) {
-                                om.readValue(record.value(), BaseMessage::class.java)
+                        val context = BeContext(
+                            startTime = Instant.now()
+                        )
+
+                        val service = when (record.topic()) {
+                            "exercise-in" -> services[0]
+                            "workout-in" -> services[1]
+                            else -> {
+                                throw Exception("Service does not exist")
                             }
-                        sendResponse(service.handleRequest(context, request))
+                        }
+
+                        try {
+                            val request =
+                                withContext(Dispatchers.IO) {
+                                    om.readValue(record.value(), BaseMessage::class.java)
+                                }
+                            sendResponse(service.handleRequest(context, request))
+                        } catch (t: Throwable) {
+                            sendResponse(service.handleError(context, t))
+                        }
                     }
-                } catch (e: Throwable) {
-                    println(e.message)
-                    sendResponse(services[0].handleError(context, e))
+                } catch (t: Throwable) {
+                    println(t.message)
                 }
             }
         } catch (ex: WakeupException) {
+            println(ex.message)
         } catch (ex: RuntimeException) {
             withContext(NonCancellable) {
                 throw ex
