734ae9e515eda7b58e8be34196ca59538ae803ff
KonstantinKhan <khan366kos@gmail.com>
2021-09-27 16:58:39 +0500

    Updated.

--- ok-workout-be-app-rabbit/src/main/kotlin/ru/otus/otuskotlin/workout/be/app/rabbit/Application.kt
+++ ok-workout-be-app-rabbit/src/main/kotlin/ru/otus/otuskotlin/workout/be/app/rabbit/Application.kt
@@ -1,30 +1,15 @@
 package ru.otus.otuskotlin.workout.be.app.rabbit
 
-import com.rabbitmq.client.CancelCallback
-import com.rabbitmq.client.ConnectionFactory
-import com.rabbitmq.client.DeliverCallback
-
 fun main() {
-    ConnectionFactory().apply {
-        host = "localhost"
-        port = 5672
-        username = "guest"
-        password = "guest"
-    }.newConnection().use { connection ->
-        connection.createChannel().use { channel ->
-            channel.queueDeclare("queue-in", false, false, false, null)
-            val deliverCallback = DeliverCallback { consumerTag, message ->
-                val messageIn = String(message.body, Charsets.UTF_8)
-                channel.basicPublish(
-                    "", "queue-out", null,
-                    "Received $messageIn".toByteArray(Charsets.UTF_8)
-                )
-            }
-            val cancelCallback = CancelCallback {}
-            channel.basicConsume("queue-in", true, deliverCallback, cancelCallback)
-            while (channel.isOpen) {
-
-            }
-        }
+    val config = RabbitConfig()
+    val exerciseProcessor by lazy {
+        RabbitExerciseProcessor(
+            config = config,
+            consumerTag = "test_exercise-tag",
+            keyIn = "exercise-in",
+            keyOut = "exercise-out",
+            exchange = "exercise-exchange",
+            exerciseService = config.exerciseService
+        )
     }
 }
