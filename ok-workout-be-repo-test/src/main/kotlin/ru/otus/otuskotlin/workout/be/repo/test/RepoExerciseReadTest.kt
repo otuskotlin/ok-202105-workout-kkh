b2dd8e08555b5e63591d96549a6907a790cfbe76
KonstantinKhan <khan366kos@gmail.com>
2021-10-09 21:53:37 +0500

    Added RepoExerciseReadTest.kt

---
+++ ok-workout-be-repo-test/src/main/kotlin/ru/otus/otuskotlin/workout/be/repo/test/RepoExerciseReadTest.kt
@@ -0,0 +1,38 @@
+package ru.otus.otuskotlin.workout.be.repo.test
+
+import kotlinx.coroutines.runBlocking
+import ru.workout.otuskotlin.workout.backend.common.models.CommonErrorModel
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseIdModel
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseModel
+import ru.workout.otuskotlin.workout.backend.common.repo.exercise.DbExerciseIdRequest
+import ru.workout.otuskotlin.workout.backend.common.repo.exercise.IRepoExercise
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+abstract class RepoExerciseReadTest {
+    abstract val repo: IRepoExercise
+
+    @Test
+    fun readSuccess() {
+        val result = runBlocking { repo.read(DbExerciseIdRequest(successId)) }
+        assertEquals(true, result.isSuccess)
+        assertEquals(readSuccessStub, result.result)
+        assertEquals(emptyList(), result.errors)
+    }
+
+    @Test
+    fun readNotFound() {
+        val result = runBlocking { repo.read(DbExerciseIdRequest(notFoundId)) }
+        assertEquals(false, result.isSuccess)
+        assertEquals(null, result.result)
+        assertEquals(listOf(CommonErrorModel(field = "id", message = "Not Found")), result.errors)
+    }
+
+    companion object : BaseInitExercise() {
+        override val initObjects: List<ExerciseModel> = listOf(createInitTestModel("read"))
+        private val readSuccessStub = initObjects.first()
+        val successId = readSuccessStub.idExercise
+        val notFoundId = ExerciseIdModel(UUID.randomUUID())
+    }
+}
