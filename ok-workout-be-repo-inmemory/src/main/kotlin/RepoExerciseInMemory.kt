bda0c988a6c92620200e38efcb1a48215e429459
KonstantinKhan <khan366kos@gmail.com>
2021-10-10 18:42:03 +0500

    Created RepoExerciseInMemory.kt and WorkoutRow.kt

---
+++ ok-workout-be-repo-inmemory/src/main/kotlin/RepoExerciseInMemory.kt
@@ -0,0 +1,86 @@
+import kotlinx.coroutines.runBlocking
+import models.ExerciseRow
+import org.ehcache.Cache
+import org.ehcache.CacheManager
+import org.ehcache.config.builders.CacheConfigurationBuilder
+import org.ehcache.config.builders.CacheManagerBuilder
+import org.ehcache.config.builders.ExpiryPolicyBuilder
+import org.ehcache.config.builders.ResourcePoolsBuilder
+import ru.workout.otuskotlin.workout.backend.common.models.CommonErrorModel
+import ru.workout.otuskotlin.workout.backend.common.models.ExerciseModel
+import ru.workout.otuskotlin.workout.backend.common.repo.common.exercise.*
+import java.time.Duration
+
+class RepoExerciseInMemory(
+    private val initObjects: List<ExerciseModel>,
+    private val ttl: Duration = Duration.ofMinutes(1)
+) : IRepoExercise {
+
+    private val cache: Cache<String, ExerciseRow> = let {
+        val cacheManager: CacheManager = CacheManagerBuilder
+            .newCacheManagerBuilder()
+            .build(true)
+
+        cacheManager.createCache(
+            "workout-exercise-cache",
+            CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(
+                    String::class.java,
+                    ExerciseRow::class.java,
+                    ResourcePoolsBuilder.heap(100)
+                )
+                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(ttl))
+                .build()
+        )
+    }
+
+    init {
+        runBlocking {
+            initObjects.forEach {
+                save(it)
+            }
+        }
+    }
+
+    private suspend fun save(item: ExerciseModel): DbExerciseResponse {
+        val row = ExerciseRow(item)
+        if (row.idExercise == null) {
+            return DbExerciseResponse(
+                result = null,
+                errors = listOf(
+                    CommonErrorModel(
+                        field = "id",
+                        message = "Id most not be null or blank"
+                    )
+                ),
+                isSuccess = false
+            )
+        }
+        cache.put(row.idExercise, row)
+        return DbExerciseResponse(
+            result = row.toInternal(),
+            isSuccess = true
+        )
+    }
+
+    override suspend fun create(req: DbExerciseModelRequest): DbExerciseResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun read(req: DbExerciseIdRequest): DbExerciseResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(req: DbExerciseModelRequest): DbExerciseResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(req: DbExerciseIdRequest): DbExerciseResponse {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun search(req: DbExerciseFilterRequest): DbExercisesResponse {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ ok-workout-be-repo-inmemory/src/main/kotlin/models/WorkoutRow.kt
@@ -0,0 +1,76 @@
+package models
+
+import ru.workout.otuskotlin.workout.backend.common.models.*
+import java.io.Serializable
+import java.time.Instant
+
+data class WorkoutRow(
+    val workoutDate: String? = null,
+    val duration: Double? = null,
+    val recoveryTime: Double? = null,
+    val modificationWorkout: String? = null,
+    val exercisesBlock: MutableList<ExercisesBlock>? = null,
+    val idWorkout: String? = null,
+) : Serializable {
+    constructor(internal: WorkoutModel) : this(
+        workoutDate = internal.workoutDate.toString(),
+        duration = internal.duration,
+        recoveryTime = internal.recoveryTime,
+        modificationWorkout = internal.modificationWorkout.name,
+        exercisesBlock = internal.exercisesBlock.map {
+            ExercisesBlock(it)
+        }.toMutableList(),
+        idWorkout = internal.idWorkout.takeIf { it != WorkoutIdModel.NONE }.toString()
+    )
+
+    fun toInternal(): WorkoutModel = WorkoutModel(
+        workoutDate = Instant.parse(workoutDate),
+        duration = duration ?: 0.0,
+        recoveryTime = recoveryTime ?: 0.0,
+        modificationWorkout = WorkoutModel.ModificationWorkout.valueOf(modificationWorkout ?: "CLASSIC"),
+        exercisesBlock = exercisesBlock?.map {
+            ExercisesBlockModel(
+                exercise = it.exercise.toInternal(),
+                sets = it.sets.map { oneSet ->
+                    OneSetModel(
+                        performance = oneSet.performance.map { performance ->
+                            PerformanceModel(
+                                weight = performance.weight,
+                                measure = PerformanceModel.Measure.valueOf(performance.measure),
+                                repetition = performance.repetition
+                            )
+                        }.toMutableList(),
+                        status = OneSetModel.Status.valueOf(oneSet.status),
+                        modificationExercise = OneSetModel.ModificationExercise.valueOf(oneSet.modificationExercise)
+                    )
+                }.toMutableList(),
+                modificationBlockExercises = ModificationBlockExercises.valueOf(it.modificationBlockExercises),
+            )
+        }?.toMutableList() ?: mutableListOf(),
+        idWorkout = idWorkout?.let { WorkoutIdModel(it) } ?: WorkoutIdModel.NONE,
+    )
+
+    class ExercisesBlock(
+        exercisesBlockModel: ExercisesBlockModel
+    ) {
+        val exercise: ExerciseRow = ExerciseRow(exercisesBlockModel.exercise)
+        val sets: MutableList<OneSet> = exercisesBlockModel.sets.map { OneSet(it) }.toMutableList()
+        val modificationBlockExercises: String = exercisesBlockModel.modificationBlockExercises.name
+    }
+
+    class OneSet(
+        oneSetModel: OneSetModel
+    ) {
+        val performance: MutableList<Performance> = oneSetModel.performance.map { Performance(it) }.toMutableList()
+        val status: String = oneSetModel.status.name
+        val modificationExercise: String = oneSetModel.modificationExercise.name
+    }
+
+    class Performance(
+        performanceModel: PerformanceModel
+    ) {
+        val weight: Double = performanceModel.weight
+        val measure: String = performanceModel.measure.name
+        val repetition: Int = performanceModel.repetition
+    }
+}
