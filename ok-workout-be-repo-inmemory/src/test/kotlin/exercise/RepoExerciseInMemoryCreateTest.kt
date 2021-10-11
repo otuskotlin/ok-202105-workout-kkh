88a71db4091d044a70fb004929d458be9a7d5cf4
KonstantinKhan <khan366kos@gmail.com>
2021-10-11 21:08:54 +0500

    Implemented of RepoExerciseInMemoryCreateTest.kt

--- ok-workout-be-repo-inmemory/src/test/kotlin/exercise/RepoExerciseInMemoryCreateTest.kt
+++ ok-workout-be-repo-inmemory/src/test/kotlin/exercise/RepoExerciseInMemoryCreateTest.kt
@@ -1,4 +1,9 @@
 package exercise
 
-class RepoExerciseInMemoryCreateTest {
+import RepoExerciseInMemory
+import ru.otus.otuskotlin.workout.be.repo.test.exercise.RepoExerciseCreateTest
+import ru.workout.otuskotlin.workout.backend.common.repo.common.exercise.IRepoExercise
+
+class RepoExerciseInMemoryCreateTest: RepoExerciseCreateTest() {
+    override val repo: IRepoExercise = RepoExerciseInMemory(initObjects = initObjects)
 }
