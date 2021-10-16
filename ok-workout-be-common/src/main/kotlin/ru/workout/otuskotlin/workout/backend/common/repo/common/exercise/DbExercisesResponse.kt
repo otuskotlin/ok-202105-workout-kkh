992ef13dfdd165c9b48652a4f18612a056b1d385
KonstantinKhan <khan366kos@gmail.com>
2021-10-16 12:42:12 +0500

    Updated DbExercisesResponse.kt

--- ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/repo/common/exercise/DbExercisesResponse.kt
+++ ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/repo/common/exercise/DbExercisesResponse.kt
@@ -6,6 +6,6 @@ import ru.workout.otuskotlin.workout.backend.common.repo.common.IDbResponse
 
 class DbExercisesResponse(
     override val isSuccess: Boolean,
-    override val errors: List<CommonErrorModel>,
+    override val errors: List<CommonErrorModel> = emptyList(),
     override val result: List<ExerciseModel>
 ) : IDbResponse<List<ExerciseModel>>
