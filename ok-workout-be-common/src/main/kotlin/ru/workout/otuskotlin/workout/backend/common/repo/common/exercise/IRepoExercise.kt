46652be7d6acdd22dd3c2eb34844a1bc33a4864e
KonstantinKhan <khan366kos@gmail.com>
2021-10-17 09:16:44 +0500

    Added object NONE in IRepoExercise.kt

--- ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/repo/common/exercise/IRepoExercise.kt
+++ ok-workout-be-common/src/main/kotlin/ru/workout/otuskotlin/workout/backend/common/repo/common/exercise/IRepoExercise.kt
@@ -6,4 +6,26 @@ interface IRepoExercise {
     suspend fun update(req: DbExerciseModelRequest): DbExerciseResponse
     suspend fun delete(req: DbExerciseIdRequest): DbExerciseResponse
     suspend fun search(req: DbExerciseFilterRequest): DbExercisesResponse
+
+    object NONE: IRepoExercise {
+        override suspend fun create(req: DbExerciseModelRequest): DbExerciseResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun read(req: DbExerciseIdRequest): DbExerciseResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun update(req: DbExerciseModelRequest): DbExerciseResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun delete(req: DbExerciseIdRequest): DbExerciseResponse {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun search(req: DbExerciseFilterRequest): DbExercisesResponse {
+            TODO("Not yet implemented")
+        }
+    }
 }
