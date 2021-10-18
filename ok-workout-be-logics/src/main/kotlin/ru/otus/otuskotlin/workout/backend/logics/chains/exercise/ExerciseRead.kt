5406e3051b9acd8f0dcbe3e121a73ad1020a172c
KonstantinKhan <khan366kos@gmail.com>
2021-10-18 14:58:52 +0500

    Added workers.

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseRead.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseRead.kt
@@ -4,11 +4,13 @@ import ICorExec
 import chain
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.excercise.exerciseReadStub
 import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
-import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
-import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
-import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
 import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.otus.otuskotlin.workout.backend.common.context.BeContext
+import ru.otus.otuskotlin.workout.backend.logics.workers.*
+import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
+import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
+import ru.otus.otuskotlin.workout.backend.logics.workers.chooseDb
+import ru.otus.otuskotlin.workout.backend.logics.workers.prepareAnswer
 
 object ExerciseRead : ICorExec<BeContext> by chain<BeContext>({
     checkOperationWorker(
@@ -17,6 +19,8 @@ object ExerciseRead : ICorExec<BeContext> by chain<BeContext>({
     )
 
     chainInitWorker("Инициализация чейна")
+
+    chooseDb(title = "Выбираем БД или STUB")
 
     exerciseReadStub(title = "Обработка стабкейса для READ")
 
@@ -32,9 +36,7 @@ object ExerciseRead : ICorExec<BeContext> by chain<BeContext>({
         }
     }
 
-
-
-    // db working
+    repoRead("Читаем объект из БД")
 
     prepareAnswer("Подготовка ответа")
 }).build()
