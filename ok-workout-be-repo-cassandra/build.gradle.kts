ee4d83fe5d65f49a63e71840dac911cfe9a8321e
KonstantinKhan <khan366kos@gmail.com>
2021-10-18 21:45:14 +0500

    Added dependencies.

--- ok-workout-be-repo-cassandra/build.gradle.kts
+++ ok-workout-be-repo-cassandra/build.gradle.kts
@@ -3,5 +3,12 @@ plugins {
 }
 
 dependencies {
+    val testContainersVersion: String by project
+
     implementation(kotlin("stdlib"))
+
+    implementation(project(":ok-workout-be-common"))
+
+    testImplementation("org.testcontainers:cassandra:$testContainersVersion")
+    testImplementation(project(":ok-workout-be-repo-test"))
 }
