3c5aef40d61c1d79ac62ad581812795646668c5a
KonstantinKhan <khan366kos@gmail.com>
2021-10-18 22:12:38 +0500

    Added dependencies.

--- ok-workout-be-repo-cassandra/build.gradle.kts
+++ ok-workout-be-repo-cassandra/build.gradle.kts
@@ -1,11 +1,23 @@
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
     val testContainersVersion: String by project
+    val cassandraDriverVersion: String by project
+    val coroutinesVersion: String by project
 
     implementation(kotlin("stdlib"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
+
+    implementation("com.datastax.oss:java-driver-core:$cassandraDriverVersion")
+    implementation("com.datastax.oss:java-driver-query-builder:$cassandraDriverVersion")
+
+    kapt("com.datastax.oss:java-driver-mapper-processor:$cassandraDriverVersion")
+    implementation("com.datastax.oss:java-driver-mapper-runtime:$cassandraDriverVersion")
 
     implementation(project(":ok-workout-be-common"))
 
