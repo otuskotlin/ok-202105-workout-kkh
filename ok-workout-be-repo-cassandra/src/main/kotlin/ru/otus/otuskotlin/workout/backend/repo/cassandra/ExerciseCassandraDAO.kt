0a1541949c450f1686dc5a3b23174d661ab00604
KonstantinKhan <khan366kos@gmail.com>
2021-10-20 14:44:16 +0500

    Created ExerciseCassandraDAO.kt

---
+++ ok-workout-be-repo-cassandra/src/main/kotlin/ru/otus/otuskotlin/workout/backend/repo/cassandra/ExerciseCassandraDAO.kt
@@ -0,0 +1,26 @@
+package ru.otus.otuskotlin.workout.backend.repo.cassandra
+
+import com.datastax.oss.driver.api.mapper.annotations.*
+import ru.otus.otuskotlin.workout.backend.common.repo.common.exercise.DbExerciseFilterRequest
+import java.util.concurrent.CompletionStage
+
+@Dao
+interface ExerciseCassandraDAO {
+    @Insert
+    fun create(dto: ExerciseCassandraDTO): CompletionStage<Unit>
+
+    @Select
+    fun read(id: String): CompletionStage<ExerciseCassandraDTO>
+
+    @Update
+    fun update(dto: ExerciseCassandraDTO): CompletionStage<Unit>
+
+    @Delete
+    fun delete(dto: ExerciseCassandraDTO): CompletionStage<Unit>
+
+    @QueryProvider(
+        providerClass = ExerciseCassandraSearchQueryProvider::class,
+        entityHelpers = [ExerciseCassandraDTO::class]
+    )
+    fun search(req: DbExerciseFilterRequest): CompletionStage<Collection<ExerciseCassandraDTO>>
+}
