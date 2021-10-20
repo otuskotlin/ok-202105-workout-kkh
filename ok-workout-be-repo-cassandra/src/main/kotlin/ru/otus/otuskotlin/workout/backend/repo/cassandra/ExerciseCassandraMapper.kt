c186cb4a3b5b3df6d4cc8b1d3e3a804e93710637
KonstantinKhan <khan366kos@gmail.com>
2021-10-20 23:14:56 +0500

    Created ExerciseCassandraMapper.kt

---
+++ ok-workout-be-repo-cassandra/src/main/kotlin/ru/otus/otuskotlin/workout/backend/repo/cassandra/ExerciseCassandraMapper.kt
@@ -0,0 +1,15 @@
+package ru.otus.otuskotlin.workout.backend.repo.cassandra
+
+import com.datastax.oss.driver.api.mapper.annotations.DaoFactory
+import com.datastax.oss.driver.api.mapper.annotations.DaoKeyspace
+import com.datastax.oss.driver.api.mapper.annotations.DaoTable
+import com.datastax.oss.driver.api.mapper.annotations.Mapper
+
+@Mapper
+interface ExerciseCassandraMapper {
+    @DaoFactory
+    fun exerciseCassandraDao(
+        @DaoKeyspace keyspace: String,
+        @DaoTable tableName: String
+    ): ExerciseCassandraDTO
+}
