552c6ae0eb967679ff2c757c90a098c937552405
KonstantinKhan <khan366kos@gmail.com>
2021-10-22 11:52:38 +0500

    Added test.

--- ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/ExerciseCrudRepoTest.kt
+++ ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/ExerciseCrudRepoTest.kt
@@ -41,4 +41,105 @@ class ExerciseCrudRepoTest {
             assertEquals(expected.executionTechnique, executionTechnique)
         }
     }
+
+    @Test
+    fun readSuccessTest() {
+        val repo = RepoExerciseInMemory(initObjects = listOf(ExerciseStub.getModelExercise()))
+        val crud = ExerciseCrud(config = ContextConfig(repoExerciseTest = repo))
+        val context = BeContext(
+            workMode = WorkMode.TEST,
+            requestExerciseId = ExerciseStub.getModelExercise().idExercise,
+            operation = BeContext.MpOperations.READ
+        )
+        runBlocking {
+            crud.read(context)
+        }
+        assertEquals(CorStatus.SUCCESS, context.status)
+        val expected = ExerciseStub.getModelExercise()
+        with(context.responseExercise) {
+            assertEquals(expected.idExercise, idExercise)
+            assertEquals(expected.title, title)
+            assertEquals(expected.description, description)
+            assertEquals(expected.targetMuscleGroup, targetMuscleGroup)
+            assertEquals(expected.synergisticMuscleGroup, synergisticMuscleGroup)
+            assertEquals(expected.executionTechnique, executionTechnique)
+        }
+    }
+
+    @Test
+    fun updateSuccessTest() {
+        val repo = RepoExerciseInMemory(initObjects = listOf(ExerciseStub.getModelExercise()))
+        val crud = ExerciseCrud(config = ContextConfig(repoExerciseTest = repo))
+        val context = BeContext(
+            workMode = WorkMode.TEST,
+            requestExercise = ExerciseStub.getModelExercise(),
+            operation = BeContext.MpOperations.UPDATE
+        )
+        runBlocking { crud.update(context) }
+        assertEquals(CorStatus.SUCCESS, context.status)
+        val expected = ExerciseStub.getModelExercise()
+        with(context.responseExercise) {
+            assertEquals(expected.idExercise, idExercise)
+            assertEquals(expected.title, title)
+            assertEquals(expected.description, description)
+            assertEquals(expected.targetMuscleGroup, targetMuscleGroup)
+            assertEquals(expected.synergisticMuscleGroup, synergisticMuscleGroup)
+            assertEquals(expected.executionTechnique, executionTechnique)
+        }
+    }
+
+    @Test
+    fun deleteSuccessTest() {
+        val repo = RepoExerciseInMemory(initObjects = listOf(ExerciseStub.getModelExercise()))
+        val crud = ExerciseCrud(config = ContextConfig(repoExerciseTest = repo))
+        val context = BeContext(
+            workMode = WorkMode.TEST,
+            requestExerciseId = ExerciseStub.getModelExercise().idExercise,
+            operation = BeContext.MpOperations.DELETE
+        )
+        runBlocking { crud.delete(context) }
+        assertEquals(CorStatus.SUCCESS, context.status)
+        val expected = ExerciseStub.getModelExercise()
+        with(context.responseExercise) {
+            assertEquals(expected.idExercise, idExercise)
+            assertEquals(expected.title, title)
+            assertEquals(expected.description, description)
+            assertEquals(expected.targetMuscleGroup, targetMuscleGroup)
+            assertEquals(expected.synergisticMuscleGroup, synergisticMuscleGroup)
+            assertEquals(expected.executionTechnique, executionTechnique)
+        }
+    }
+
+    @Test
+    fun searchSuccessTest() {
+        val repo = RepoExerciseInMemory(
+            initObjects = listOf(
+                ExerciseStub.getModelExercise() {
+                    idExercise = ExerciseIdModel("eID:0001")
+                    title = "жим штанги"
+                },
+                ExerciseStub.getModelExercise() {
+                    idExercise = ExerciseIdModel("eID:0002")
+                    title = "жим гантелей"
+                },
+                ExerciseStub.getModelExercise() {
+                    idExercise = ExerciseIdModel("eID:0003")
+                    title = "подтягивания на перекладине"
+                },
+                ExerciseStub.getModelExercise() {
+                    idExercise = ExerciseIdModel("eID:0004")
+                }
+            )
+        )
+        val crud = ExerciseCrud(config = ContextConfig(repoExerciseTest = repo))
+        val context = BeContext(
+            workMode = WorkMode.TEST,
+            requestExerciseId = ExerciseStub.getModelExercise().idExercise,
+            requestSearchExercise = "жим",
+            operation = BeContext.MpOperations.SEARCH
+        )
+        runBlocking { crud.search(context) }
+        assertEquals(CorStatus.SUCCESS, context.status)
+        assertEquals(2, context.foundExercises.size)
+    }
 }
