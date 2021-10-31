dfdbe5a4d8d69332f3d8fcdc47b7e510b7762414
KonstantinKhan <khan366kos@gmail.com>
2021-10-31 12:02:35 +0500

    Removed unused import.

--- ok-workout-be-repo-inmemory/src/main/kotlin/ru/otus/otuskotlin/workout/be/repo/inmemory/models/RepoExerciseInMemory.kt
+++ ok-workout-be-repo-inmemory/src/main/kotlin/ru/otus/otuskotlin/workout/be/repo/inmemory/models/RepoExerciseInMemory.kt
@@ -14,7 +14,6 @@ import org.ehcache.config.builders.ResourcePoolsBuilder
 import ru.otus.otuskotlin.workout.backend.common.models.CommonErrorModel
 import ru.otus.otuskotlin.workout.backend.common.models.ExerciseIdModel
 import ru.otus.otuskotlin.workout.backend.common.models.ExerciseModel
-import ru.otus.otuskotlin.workout.backend.common.repo.common.exercise.*
 import ru.otus.otuskotlin.workout.backend.repo.common.exercise.*
 import java.time.Duration
 import java.util.*
