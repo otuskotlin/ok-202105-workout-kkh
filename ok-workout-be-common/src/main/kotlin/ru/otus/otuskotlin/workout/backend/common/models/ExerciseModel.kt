23155658938d4e43480da8b697d0f95000de7924
KonstantinKhan <khan366kos@gmail.com>
2021-10-31 13:16:54 +0500

    Added authorId.

--- ok-workout-be-common/src/main/kotlin/ru/otus/otuskotlin/workout/backend/common/models/ExerciseModel.kt
+++ ok-workout-be-common/src/main/kotlin/ru/otus/otuskotlin/workout/backend/common/models/ExerciseModel.kt
@@ -3,6 +3,7 @@ package ru.otus.otuskotlin.workout.backend.common.models
 data class ExerciseModel(
     var title: String = "",
     var description: String = "",
+    var authorId: AuthorIdModel = AuthorIdModel.NONE,
     var targetMuscleGroup: MutableList<String> = mutableListOf(),
     var synergisticMuscleGroup: MutableList<String> = mutableListOf(),
     var executionTechnique: String = "",
