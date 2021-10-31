8d57ec43e163f7e6ee7e165b2ad5e138a5b8a033
KonstantinKhan <khan366kos@gmail.com>
2021-10-31 11:33:42 +0500

    Created ChainPermissoins.kt

---
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/workers/ChainPermissoins.kt
@@ -0,0 +1,47 @@
+package ru.otus.otuskotlin.workout.backend.logics.workers
+
+import ICorChainDsl
+import handlers.worker
+import ru.otus.otuskotlin.workout.backend.common.context.BeContext
+import ru.otus.otuskotlin.workout.backend.common.context.CorStatus
+import ru.otus.otuskotlin.workout.backend.common.models.MpUserGroups
+import ru.otus.otuskotlin.workout.backend.common.models.MpUserPermissions
+
+fun ICorChainDsl<BeContext>.chainPermissions(title: String) = worker {
+    this.title = title
+    description = "Вычисление прав доступа для групп пользователй"
+    on {
+        status == CorStatus.RUNNING
+    }
+    handle {
+        val permAdd: Set<MpUserPermissions> = principal.groups.map {
+            when (it) {
+                MpUserGroups.USER -> setOf(
+                    MpUserPermissions.CREATE_OWN,
+                    MpUserPermissions.READ_OWN,
+                    MpUserPermissions.READ_PUBLIC,
+                    MpUserPermissions.UPDATE_OWN,
+                    MpUserPermissions.DELETE_OWN,
+                    MpUserPermissions.SEARCH_OWN,
+                    MpUserPermissions.SEARCH_PUBLIC
+                )
+                MpUserGroups.ADMIN -> setOf()
+                MpUserGroups.TEST -> setOf()
+                MpUserGroups.BAN -> setOf()
+            }
+        }.flatten().toSet()
+        val permDel: Set<MpUserPermissions> = principal.groups.map {
+            when (it) {
+                MpUserGroups.USER -> setOf()
+                MpUserGroups.ADMIN -> setOf()
+                MpUserGroups.TEST -> setOf()
+                MpUserGroups.BAN -> setOf(
+                    MpUserPermissions.CREATE_OWN,
+                    MpUserPermissions.UPDATE_OWN,
+                )
+            }
+        }.flatten().toSet()
+        chainPermissions.addAll(permAdd)
+        chainPermissions.removeAll(permDel)
+    }
+}
