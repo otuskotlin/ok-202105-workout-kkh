f520c4e118e1e9286f79e4022976472696a97551
KonstantinKhan <khan366kos@gmail.com>
2021-10-31 11:31:08 +0500

    Updated database selection based on user groups.

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/workers/ChooseDb.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/workers/ChooseDb.kt
@@ -4,6 +4,7 @@ import handlers.CorChainDsl
 import handlers.worker
 import ru.otus.otuskotlin.workout.backend.common.context.BeContext
 import ru.otus.otuskotlin.workout.backend.common.models.MpStubCases
+import ru.otus.otuskotlin.workout.backend.common.models.MpUserGroups
 import ru.otus.otuskotlin.workout.backend.common.models.WorkMode
 import ru.otus.otuskotlin.workout.backend.repo.common.exercise.IRepoExercise
 
@@ -11,6 +12,10 @@ internal fun CorChainDsl<BeContext>.chooseDb(title: String) = worker {
     this.title = title
     description = ""
     handle {
+        if (principal.groups.contains(MpUserGroups.TEST)) {
+            exerciseRepo = config.repoExerciseTest
+            return@handle
+        }
         exerciseRepo = when (workMode) {
             WorkMode.PROD -> config.repoExerciseProd
             WorkMode.TEST -> config.repoExerciseTest
