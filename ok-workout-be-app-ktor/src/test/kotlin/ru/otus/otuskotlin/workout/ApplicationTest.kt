f7a073a0f209e51f9f487d3e528359f1a3b4ea88
KonstantinKhan <khan366kos@gmail.com>
2021-10-31 10:43:07 +0500

    Updated module.

--- ok-workout-be-app-ktor/src/test/kotlin/ru/otus/otuskotlin/workout/ApplicationTest.kt
+++ ok-workout-be-app-ktor/src/test/kotlin/ru/otus/otuskotlin/workout/ApplicationTest.kt
@@ -4,11 +4,12 @@ import io.ktor.http.*
 import io.ktor.application.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+import ru.otus.otuskotlin.workout.configs.AppKtorConfig
 
 class ApplicationTest {
     @Test
     fun `root endpoint`() {
-        withTestApplication(Application::module) {
+        withTestApplication({ module(config = AppKtorConfig()) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello, Ktor!", response.content)
