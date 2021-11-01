df3c1f9a35468a62159ea8d66b12bc67ce884180
KonstantinKhan <khan366kos@gmail.com>
2021-11-01 14:56:31 +0500

    Added chains and workers into ExerciseRead.kt.

--- ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseRead.kt
+++ ok-workout-be-logics/src/main/kotlin/ru/otus/otuskotlin/workout/backend/logics/chains/exercise/ExerciseRead.kt
@@ -2,10 +2,12 @@ package ru.otus.otuskotlin.workout.backend.logics.chains.exercise
 
 import ICorExec
 import chain
+import handlers.worker
 import ru.otus.otuskotlin.workout.backend.logics.chains.stubs.excercise.exerciseReadStub
 import ru.otus.otuskotlin.workout.backend.logics.helpers.validationLogics
 import ru.otus.otuskotlin.workout.validation.validators.StringNonEmptyValidator
 import ru.otus.otuskotlin.workout.backend.common.context.BeContext
+import ru.otus.otuskotlin.workout.backend.common.context.CorStatus
 import ru.otus.otuskotlin.workout.backend.logics.workers.*
 import ru.otus.otuskotlin.workout.backend.logics.workers.chainInitWorker
 import ru.otus.otuskotlin.workout.backend.logics.workers.checkOperationWorker
@@ -32,7 +34,18 @@ object ExerciseRead : ICorExec<BeContext> by chain<BeContext>({
         }
     }
 
+    chainPermissions("Вычисление разрешений для пользователя")
     repoRead("Читаем объект из БД")
+    accessValidation("Вычисление прав доступа")
+
+    worker {
+        title = "Подготовка результата к отправке"
+        description = title
+        on { status == CorStatus.RUNNING }
+        handle { responseExercise = dbExercise }
+    }
+
+    frontPermissions(title = "Вычисление пользовательских разрешений для фронтенда")
 
     prepareAnswer("Подготовка ответа")
 }).build()
