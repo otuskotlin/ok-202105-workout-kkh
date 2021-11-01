4ea4aabf6ee549726036a51824dfb167bdc2681b
KonstantinKhan <khan366kos@gmail.com>
2021-11-01 14:55:16 +0500

    Created ExerciseCrudAuthTest.kt

---
+++ ok-workout-be-logics/src/test/kotlin/ru/otus/otuskotlin/workout/backend/logics/auth/ExerciseCrudAuthTest.kt
@@ -0,0 +1,16 @@
+package ru.otus.otuskotlin.workout.backend.logics.auth
+
+import kotlin.test.Test
+
+class ExerciseCrudAuthTest {
+
+    @Test
+    fun createSuccessTest() {
+
+    }
+
+    @Test
+    fun readSuccessTest() {
+
+    }
+}
