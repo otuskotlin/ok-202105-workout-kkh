a88372a9ca03950dd63bbd7bfba8dd38c5b088f3
KonstantinKhan <khan366kos@gmail.com>
2021-11-02 13:41:36 +0500

    Added authorId into ExerciseCrudAuthTest.kt

--- ok-workout-be-repo-inmemory/src/main/kotlin/ru/otus/otuskotlin/workout/be/repo/inmemory/models/ExerciseRow.kt
+++ ok-workout-be-repo-inmemory/src/main/kotlin/ru/otus/otuskotlin/workout/be/repo/inmemory/models/ExerciseRow.kt
@@ -1,5 +1,6 @@
 package ru.otus.otuskotlin.workout.be.repo.inmemory.models
 
+import ru.otus.otuskotlin.workout.backend.common.models.AuthorIdModel
 import ru.otus.otuskotlin.workout.backend.common.models.ExerciseIdModel
 import ru.otus.otuskotlin.workout.backend.common.models.ExerciseModel
 import java.io.Serializable
@@ -7,6 +8,7 @@ import java.io.Serializable
 data class ExerciseRow(
     val title: String? = null,
     val description: String? = null,
+    val authorId: String? = null,
     val targetMuscleGroup: MutableList<String>? = null,
     val synergisticMuscleGroup: MutableList<String>? = null,
     val executionTechnique: String? = null,
@@ -15,6 +17,7 @@ data class ExerciseRow(
     constructor(internal: ExerciseModel) : this(
         title = internal.title.takeIf { it.isNotBlank() },
         description = internal.description.takeIf { it.isNotBlank() },
+        authorId = internal.authorId.takeIf { it != AuthorIdModel.NONE }?.asString(),
         targetMuscleGroup = internal.targetMuscleGroup.takeIf { it.isNotEmpty() },
         synergisticMuscleGroup = internal.synergisticMuscleGroup.takeIf { it.isNotEmpty() },
         executionTechnique = internal.executionTechnique.takeIf { it.isNotBlank() },
@@ -24,6 +27,7 @@ data class ExerciseRow(
     fun toInternal(): ExerciseModel = ExerciseModel(
         title = title ?: "",
         description = description ?: "",
+        authorId = authorId?.let { AuthorIdModel(it) } ?: AuthorIdModel.NONE,
         targetMuscleGroup = targetMuscleGroup ?: mutableListOf(),
         synergisticMuscleGroup = synergisticMuscleGroup ?: mutableListOf(),
         executionTechnique = executionTechnique ?: "",
