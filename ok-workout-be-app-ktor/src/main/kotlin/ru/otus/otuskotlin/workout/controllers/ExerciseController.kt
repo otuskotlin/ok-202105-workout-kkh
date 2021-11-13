12c8683808acb99501b8a664854be91a9147b909
KonstantinKhan <khan366kos@gmail.com>
2021-11-13 22:22:54 +0500

    Added logging.

--- ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/ExerciseController.kt
+++ ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/controllers/ExerciseController.kt
@@ -3,41 +3,60 @@ package ru.otus.otuskotlin.workout.controllers
 import ru.otus.otuskotlin.workout.be.service.openapi.exceptions.ExerciseService
 import io.ktor.application.*
 import ru.otus.otuskotlin.workout.helpers.handleRoute
+import ru.otus.otuskotlin.workout.logging.mpLogger
 import ru.otus.otuskotlin.workout.openapi.models.*
 
 suspend fun ApplicationCall.initExercise(exerciseService: ExerciseService) {
 
-    handleRoute<InitExerciseRequest, InitExerciseResponse> { request ->
+    handleRoute<InitExerciseRequest, InitExerciseResponse>(
+        "init-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.initExercise(this, request)
     }
 }
 
 suspend fun ApplicationCall.createExercise(exerciseService: ExerciseService) {
-    handleRoute<CreateExerciseRequest, CreateExerciseResponse> { request ->
+    handleRoute<CreateExerciseRequest, CreateExerciseResponse>(
+        "create-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.createExercise(this, request)
     }
 }
 
 suspend fun ApplicationCall.readExercise(exerciseService: ExerciseService) {
-    handleRoute<ReadExerciseRequest, ReadExerciseResponse> { request ->
+    handleRoute<ReadExerciseRequest, ReadExerciseResponse>(
+        "read-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.readExercise(this, request)
     }
 }
 
 suspend fun ApplicationCall.updateExercise(exerciseService: ExerciseService) {
-    handleRoute<UpdateExerciseRequest, UpdateExerciseResponse> { request ->
+    handleRoute<UpdateExerciseRequest, UpdateExerciseResponse>(
+        "update-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.updateExercise(this, request)
     }
 }
 
 suspend fun ApplicationCall.deleteExercise(exerciseService: ExerciseService) {
-    handleRoute<DeleteExerciseRequest, DeleteExerciseResponse> { request ->
+    handleRoute<DeleteExerciseRequest, DeleteExerciseResponse>(
+        "delete-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.deleteExercise(this, request)
     }
 }
 
 suspend fun ApplicationCall.searchExercise(exerciseService: ExerciseService) {
-    handleRoute<SearchExerciseRequest, SearchExerciseResponse> { request ->
+    handleRoute<SearchExerciseRequest, SearchExerciseResponse>(
+        "search-exercise",
+        mpLogger(this::class.java)
+    ) { request ->
         exerciseService.searchExercise(this, request)
     }
 }
--- ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/helpers/HandleRoute.kt
+++ ok-workout-be-app-ktor/src/main/kotlin/ru/otus/otuskotlin/workout/helpers/HandleRoute.kt
@@ -7,23 +7,28 @@ import io.ktor.request.*
 import io.ktor.response.*
 import ru.otus.otuskotlin.workout.openapi.models.BaseMessage
 import ru.otus.otuskotlin.workout.backend.common.context.BeContext
+import ru.otus.otuskotlin.workout.logging.MpLogWrapper
 import ru.otus.otuskotlin.workout.mappers.toModel
 import java.time.Instant
 
 suspend inline fun <reified T : BaseMessage, reified U : BaseMessage> ApplicationCall.handleRoute(
-    block: BeContext.(T) -> U
+    logId: String,
+    logger: MpLogWrapper,
+    crossinline block: suspend BeContext.(T) -> U
 ) {
-    val request = receive<BaseMessage>() as T
-    val context = BeContext(
-        startTime = Instant.now(),
-        principal = principal<JWTPrincipal>().toModel()
-    )
-    try {
-        val response = context.block(request)
-        respond(response)
-    } catch (e: Exception) {
-        context.addError(e)
-        val response = context.block(request)
-        respond(response)
+    logger.doWithLogging(logId) {
+        val request = receive<BaseMessage>() as T
+        val context = BeContext(
+            startTime = Instant.now(),
+            principal = principal<JWTPrincipal>().toModel()
+        )
+        try {
+            val response = context.block(request)
+            respond(response)
+        } catch (e: Exception) {
+            context.addError(e)
+            val response = context.block(request)
+            respond(response)
+        }
     }
 }
