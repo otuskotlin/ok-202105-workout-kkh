4eeecf1de70e7af3999a65620f444bde8beb5040
KonstantinKhan <khan366kos@gmail.com>
2021-11-13 22:24:30 +0500

    Added logging.

--- ok-workout-be-service-openapi/src/main/kotlin/ru/otus/otuskotlin/workout/be/service/openapi/exceptions/ExerciseService.kt
+++ ok-workout-be-service-openapi/src/main/kotlin/ru/otus/otuskotlin/workout/be/service/openapi/exceptions/ExerciseService.kt
@@ -1,13 +1,16 @@
 package ru.otus.otuskotlin.workout.be.service.openapi.exceptions
 
+import org.slf4j.event.Level
 import ru.otus.otuskotlin.workout.backend.logics.ExerciseCrud
 import ru.otus.otuskotlin.workout.openapi.models.*
 import ru.otus.otuskotlin.workout.backend.common.context.BeContext
 import ru.otus.otuskotlin.workout.backend.mapping.openapi.*
+import ru.otus.otuskotlin.workout.logging.mpLogger
 
 class ExerciseService(
     private var crud: ExerciseCrud
 ) : IHandlerRequests {
+    private val logger = mpLogger(this::class.java)
 
     override suspend fun handleRequest(context: BeContext, request: BaseMessage) = try {
         when (request) {
@@ -23,39 +26,44 @@ class ExerciseService(
         handleError(context, t)
     }
 
-    fun initExercise(context: BeContext, request: InitExerciseRequest): InitExerciseResponse {
+    suspend fun initExercise(context: BeContext, request: InitExerciseRequest): InitExerciseResponse {
         context.setQuery(request)
-        return context.toInitExerciseResponse()
+        return context.handle("init-exercise", BeContext::toInitExerciseResponse)
     }
 
     suspend fun createExercise(context: BeContext, request: CreateExerciseRequest): CreateExerciseResponse {
-        crud.create(context.setQuery(request))
-        return context.toCreateExerciseResponse()
+        return context.handle("create-exercise", BeContext::toCreateExerciseResponse) {
+            crud.create(context.setQuery(request))
+        }
     }
 
     suspend fun readExercise(context: BeContext, request: ReadExerciseRequest): ReadExerciseResponse {
-        crud.read(context.setQuery(request))
-        return context.toReadExerciseResponse()
+        return context.handle("read-exercise", BeContext::toReadExerciseResponse) {
+            crud.read(context.setQuery(request))
+        }
     }
 
     suspend fun updateExercise(context: BeContext, request: UpdateExerciseRequest): UpdateExerciseResponse {
-        crud.update(context.setQuery(request))
-        return context.toUpdateExerciseResponse()
+        return context.handle("update-exercise", BeContext::toUpdateExerciseResponse) {
+            crud.update(context.setQuery(request))
+        }
     }
 
     suspend fun deleteExercise(context: BeContext, request: DeleteExerciseRequest): DeleteExerciseResponse {
-        crud.delete(context.setQuery(request))
-        return context.toDeleteExerciseResponse()
+        return context.handle("delete-exercise", BeContext::toDeleteExerciseResponse) {
+            crud.delete(context.setQuery(request))
+        }
     }
 
     suspend fun searchExercise(context: BeContext, request: SearchExerciseRequest): SearchExerciseResponse {
-        crud.search(context.setQuery(request))
-        return context.toSearchExerciseResponse()
+        return context.handle("search-exercise", BeContext::toSearchExerciseResponse) {
+            crud.search(context.setQuery(request))
+        }
     }
 
     override suspend fun handleError(context: BeContext, e: Throwable): BaseMessage {
         context.addError(e)
-        return context.toReadExerciseResponse()
+        return context.handle("error-exercise", BeContext::toReadExerciseResponse)
     }
 
     override suspend fun finish(context: BeContext) {
@@ -63,5 +71,25 @@ class ExerciseService(
 
     fun userDisconnected(context: BeContext) {
         TODO("Not yet implemented")
+    }
+
+    private suspend fun <T> BeContext.handle(
+        logId: String,
+        mapper: BeContext.() -> T,
+        block: suspend (BeContext) -> Unit = {}
+    ): T {
+        logger.log(
+            msg = "Request got, query = {}",
+            level = Level.INFO,
+            data = toLog("$logId-request-got")
+        )
+        block(this)
+        return mapper().also {
+            logger.log(
+                msg = "Response ready, response = {}",
+                level = Level.INFO,
+                data = toLog("$logId-request-handled")
+            )
+        }
     }
 }
